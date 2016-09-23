fabf646359ce831778a8fc4364629f5a1353b3d9
TakahashiIkki <new_ittyandesu@outlook.jp>
2016-09-24 08:55:27 +0900

    Gridを作成する関数を作成

--- SampleGridView/ViewController.swift
+++ SampleGridView/ViewController.swift
@@ -10,16 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blackView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        self.drawGridToView(targetView: blackView, numberOfRows: 2, numberOfColumns: 2, lineColor: UIColor.white, lineWidth: 1.0)
+    }
+    
+    func drawGridToView(targetView: UIView, numberOfRows: Int, numberOfColumns: Int, lineColor: UIColor, lineWidth: CGFloat) {
 
-
+        let columnWidth = targetView.frame.width / CGFloat(numberOfRows + 1)
+        let columnHeight = targetView.frame.height / CGFloat(numberOfColumns + 1)
+        
+        let gridLayer = CAShapeLayer()
+        gridLayer.frame = targetView.bounds
+        gridLayer.strokeColor = lineColor.cgColor
+        gridLayer.lineWidth = lineWidth
+        
+        let path = CGMutablePath()
+        
+        for i in 1...numberOfColumns {
+            let startPoint = CGPoint(x: targetView.bounds.origin.x, y: columnHeight * CGFloat(i))
+            let endPoint   = CGPoint(x: targetView.frame.width, y: columnHeight * CGFloat(i))
+            path.move(to: startPoint)
+            path.addLine(to: endPoint)
+            gridLayer.path = path
+        }
+        
+        for i in 1...numberOfRows {
+            let startPoint = CGPoint(x: columnWidth * CGFloat(i), y: targetView.bounds.origin.y)
+            let endPoint   = CGPoint(x: columnWidth * CGFloat(i), y: targetView.frame.height)
+            path.move(to: startPoint)
+            path.addLine(to: endPoint)
+            gridLayer.path = path
+        }
+        
+        targetView.layer.addSublayer(gridLayer)
+    }
+    
 }
 
